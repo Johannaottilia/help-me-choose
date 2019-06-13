773281fb7ad27026394ff7d601a55af0c88815b5
Johanna Nyberg <johanna.nyberg@live.com>
2019-06-13 22:15:21 +0200

    Add jQuery and simple script and more styling

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.resultSection').hide()
+  var add_option = function() {
+
+    $('.inputs').append('<input></input>')
+  }
+
+  $('.addOption').click(add_option)
+
+})
